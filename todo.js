f595161068cb4840e1ced0e9da510b7145922483
taurikirsipuu <46881542+taurikirsipuu@users.noreply.github.com>
2023-03-14 05:09:05 +0200

    5-tund - todo rakenduse algus.

---
+++ todo.js
@@ -0,0 +1,30 @@
+class Entry{
+    constructor(title, description, date){
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.done = false;
+    }
+}
+
+class Todo{
+    constructor(){
+        this.entries = [];
+
+        document.querySelector('#addButton').addEventListener('click', () => {this.addEntry();});
+    }
+
+    addEntry(){
+        const titleValue = document.querySelector('#title').value;
+        const descriptionValue = document.querySelector('#description').value;
+        const dateValue = document.querySelector('#date').value;
+
+        this.entries.push(new Entry(titleValue, descriptionValue, dateValue));
+
+        console.log(this.entries);
+    }
+
+}
+
+const todo = new Todo();
+
